632d48d08dcefbbf1571eec4c0bfd8e142e3be3c
Danil <danillihinin92@gmail.com>
2026-09-15 20:06:27 +0300

    fix fields in ApartmentExportDto.cs

--- WebApiBooking/WebApiBooking.ConsoleApp/DTOs/ApartmentExportDto.cs
+++ WebApiBooking/WebApiBooking.ConsoleApp/DTOs/ApartmentExportDto.cs
@@ -6,6 +6,8 @@ public record ApartmentExportDto
 {
     public int Id { get; init; }
     public string Name { get; init; }
-    [JsonRequired]public decimal Price { get; init; }
-    [JsonRequired]public short Rooms { get; init; } 
+    [JsonRequired]
+    public decimal Price { get; init; }
+    [JsonRequired]
+    public short Rooms { get; init; } 
 }
